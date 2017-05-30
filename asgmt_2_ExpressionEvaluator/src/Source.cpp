8cdf678d7bd0e2e388c7b9279d9d35fb5153ddf9
Uthmanhere <uthmanhere@gmail.com>
2017-05-30 17:06:04 +0500

    Add files via upload

---
+++ asgmt_2_ExpressionEvaluator/src/Source.cpp
@@ -0,0 +1,57 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Source.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  26/05/2017 16:31:04
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Muhammad Usman (N12), uthmanhere@gmail.com
+ *   Organization:  SEECS, NUST
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+#include "Calc.hpp"
+#include "customException.cpp"
+
+using namespace std;
+
+int main()
+{
+	string expression;
+
+	do
+	{
+		cout << "> Expression :\t";
+		getline(cin, expression);
+		Calc cal(expression);
+		try
+		
+		{
+			cout << "\t"<< "\e[1m"
+				<< cal.manip() 
+				<< "\e[0m" 
+				<< endl << endl;
+		}
+		catch (exception& e)
+		{
+			cerr << "Exception!\t" << "what?\t" 
+				<< "\e[1m"
+				<< e.what() 
+				<< "\e[0m"
+				<< endl << endl;
+		}
+
+	} while (1);
+
+	return 0;
+}
